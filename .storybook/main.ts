49c63e194b606359485871c9fc18731f372a3ce1
Femina <femina.brahmbhatt@telus.com>
2025-04-23 13:35:27 +0530

    Added storybook accessbility testing a11y to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,51 +8,13 @@ const config: StorybookConfig = {
     "@storybook/preset-create-react-app",
     "@storybook/addon-interactions",
     "@storybook/addon-links",
+    "@storybook/addon-a11y"
   ],
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
   },
   staticDirs: ["../public"],
-  // webpackFinal: async (config: Configuration) => {
-  //   // Ensure config.module exists and has rules
-  //   if (config.module) {
-  //     if (!config.module.rules) {
-  //       config.module.rules = [];
-  //     }
-
-  //     // Add TypeScript support with ts-loader
-  //     config.module.rules.push({
-  //       test: /\.css$/,
-  //       use: [
-  //         "style-loader",
-  //         "css-loader",
-  //         {
-  //           loader: "postcss-loader",
-  //           options: {
-  //             postcssOptions: {
-  //               plugins: [require("tailwindcss"), require("autoprefixer")],
-  //             },
-  //           },
-  //         },
-  //       ],
-  //       include: /src/,
-  //     },{
-  //       test: /\.tsx?$/,  // Match TypeScript files
-  //       use: [
-  //         {
-  //           loader: 'ts-loader',
-  //           options: {
-  //             transpileOnly: true,  // Skip type checking for faster builds
-  //           },
-  //         },
-  //       ],
-  //     });
-  //   }
-
-  //   // Return the modified config
-  //   return config;
-  // },
 };
 
 export default config;
